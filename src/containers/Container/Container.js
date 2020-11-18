9c61dec856355ea4ab9c9dababfb6983b3ae4dee
Michal Orkisz <xxmichas@gmail.com>
2020-11-18 13:20:55 +0000

    x

---
+++ src/containers/Container/Container.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Container.module.css'
+
+const Container = () => {
+    return (
+        <div className={styles.Container}>
+            dsadsa
+        </div>
+    )
+}
+
+export default Container
---
+++ src/containers/Fluid/Fluid.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import styles from './Fluid.module.css'
+
+export class Fluid extends Component {
+    componentDidMount = () => {
+        const script = document.createElement("script");
+        script.src = process.env.PUBLIC_URL + '/script.js'
+        script.async = true;
+        document.body.appendChild(script);
+    }
+    render() {
+        return (
+            <canvas className={styles.Canvas} />
+        )
+    }
+}
+
+export default Fluid
