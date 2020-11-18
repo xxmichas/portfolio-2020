67991e8bb10dc86ec3c24916261e0fbfb5f04456
Michal Orkisz <xxmichas@gmail.com>
2020-11-18 16:58:21 +0000

    x

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react'
 import Container from './containers/Container/Container'
 
 const App = () => {
     return (
-        <div>
-            <Container />
-            <Container />
-            <Container />
-            <Container />
+        <div style={{position: "relative"}}>
+            <Container>111</Container>
+            <Container>222</Container>
+            <Container>333</Container>
+            <Container>444</Container>
         </div>
     )
 }
--- src/containers/Container/Container.js
+++ src/containers/Container/Container.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from './Container.module.css'
 
-const Container = () => {
+const Container = (props) => {
     return (
         <div className={styles.Container}>
-            dsadsa
+            {props.children}
         </div>
     )
 }
