3860ea1800e5c634a6d4d11cd31ae7b248ad3dc2
Michal Orkisz <xxmichas@gmail.com>
2020-11-19 08:20:04 +0000

    x

--- src/containers/ProfileCard/ProfileCard.js
+++ src/containers/ProfileCard/ProfileCard.js
@@ -11,50 +11,6 @@ import Twitter from '../../assets/social/twitter.png'
 
 const ProfileCard = () => {
     const [Hovered, setHovered] = useState(false)
-    const [showSocial, setShowSocial] = useState({email: false, github: false, discord: false, steam: false, twitter: false})
-
-    const ShowSocialHandler = (abort) => {
-        let Timeouts = []
-        let Interval = 100
-        if (abort === true) {
-            Timeouts.forEach(el => {
-                clearTimeout(el)
-            });
-            Timeouts.length = 0
-            setShowSocial({email: false, github: false, discord: false, steam: false, twitter: false})
-        }
-        else{
-            let e = setTimeout(() => {
-                setShowSocial(prevState => ({...prevState, email: true}))
-                Timeouts.splice(Timeouts.indexOf(e), 1)
-            }, Interval * 1)
-            Timeouts.push(e)
-            let g = setTimeout(() => {
-                setShowSocial(prevState => ({...prevState, github: true}))
-                Timeouts.splice(Timeouts.indexOf(g), 1)
-            }, Interval * 2)
-            Timeouts.push(g)
-            let d = setTimeout(() => {
-                setShowSocial(prevState => ({...prevState, discord: true}))
-                Timeouts.splice(Timeouts.indexOf(d), 1)
-            }, Interval * 3)
-            Timeouts.push(d)
-            let s = setTimeout(() => {
-                setShowSocial(prevState => ({...prevState, steam: true}))
-                Timeouts.splice(Timeouts.indexOf(s), 1)
-            }, Interval * 4)
-            Timeouts.push(s)
-            let t = setTimeout(() => {
-                setShowSocial(prevState => ({...prevState, twitter: true}))
-                Timeouts.splice(Timeouts.indexOf(t), 1)
-            }, Interval * 5)
-            Timeouts.push(t)
-            console.log(Timeouts)
-            setTimeout(() => {
-                console.log(Timeouts)
-            }, 330);
-        }
-    }
 
     return (
         <Fragment>
@@ -74,7 +30,6 @@ const ProfileCard = () => {
                     exitDone: styles.OverlayExitDone
                 }} timeout={300}>
                     {state => {
-                        //console.log(state)
                         return (
                             <div className={styles.Overlay}>
                                 <CSSTransition in={state === "entered"} classNames={{
@@ -82,52 +37,47 @@ const ProfileCard = () => {
                                     enterDone: styles.IconsEnterDone,
                                     exitActive: styles.IconsExitActive,
                                     exitDone: styles.IconsExitDone
-                                }} timeout={250} onEntered={() => {
-                                    ShowSocialHandler(false)
-                                }} onExit={() => {
-                                    ShowSocialHandler(true)
-                                }}>
+                                }} timeout={250}>
                                     {state2 => {
-                                        console.log(`state 2: ${state2}`)
                                         return (
                                             <div className={styles.Icons}>
-                                                <CSSTransition in={showSocial.email} classNames={{
+                                                <CSSTransition in={state2 === "entered"} classNames={{
                                                     enterActive: styles.SocialsEnterActive,
-                                                    enterDone: styles.SocialsEnterDone,
+                                                    enterDone: styles.SocialsEnterActive,
                                                     exitActive: styles.SocialsExitActive,
-                                                    exitDone: styles.SocialsExitDone
+                                                    exitDone: styles.SocialsExitActive
                                                 }} timeout={300}>
                                                     <div className={styles.Socials}><img src={Gmail} alt="Email" /><p>xxmichas@gmail.com</p></div>
                                                 </CSSTransition>
-                                                <CSSTransition in={showSocial.github} classNames={{
+                                                <CSSTransition in={state2 === "entered"} classNames={{
                                                     enterActive: styles.SocialsEnterActive,
-                                                    enterDone: styles.SocialsEnterDone,
+                                                    enterDone: styles.SocialsEnterActive,
                                                     exitActive: styles.SocialsExitActive,
-                                                    exitDone: styles.SocialsExitDone
+                                                    exitDone: styles.SocialsExitActive
                                                 }} timeout={300}>
                                                     <div className={styles.Socials}><img src={Github} alt="Github" /><p>/xxmichas</p></div>
                                                 </CSSTransition>
-                                                <CSSTransition in={showSocial.discord} classNames={{
+                                                <CSSTransition in={state2 === "entered"} classNames={{
                                                     enterActive: styles.SocialsEnterActive,
-                                                    enterDone: styles.SocialsEnterDone,
+                                                    enterDone: styles.SocialsEnterActive,
                                                     exitActive: styles.SocialsExitActive,
-                                                    exitDone: styles.SocialsExitDone
+                                                    exitDone: styles.SocialsExitActive
                                                 }} timeout={300}>
                                                     <div className={styles.Socials}><img src={Discord} alt="Discord" /><p>xxmichas#0499</p></div>
                                                 </CSSTransition>
-                                                <CSSTransition in={showSocial.steam} classNames={{
+                                                <CSSTransition in={state2 === "entered"} classNames={{
                                                     enterActive: styles.SocialsEnterActive,
-                                                    enterDone: styles.SocialsEnterDone,
+                                                    enterDone: styles.SocialsEnterActive,
                                                     exitActive: styles.SocialsExitActive,
-                                                    exitDone: styles.SocialsExitDone
+                                                    exitDone: styles.SocialsExitActive
                                                 }} timeout={300}>
                                                     <div className={styles.Socials}><img src={Steam} alt="Steam" /><p>xxmichas</p></div>
                                                 </CSSTransition>
-                                                <CSSTransition in={showSocial.twitter} classNames={{
+                                                <CSSTransition in={state2 === "entered"} classNames={{
                                                     enterActive: styles.SocialsEnterActive,
-                                                    enterDone: styles.SocialsEnterDone,
+                                                    enterDone: styles.SocialsEnterActive,
                                                     exitActive: styles.SocialsExitActive,
-                                                    exitDone: styles.SocialsExitDone
+                                                    exitDone: styles.SocialsExitActive
                                                 }} timeout={300}>
                                                     <div className={styles.Socials}><img src={Twitter} alt="Twitter" /><p>@xxmichas</p></div>
                                                 </CSSTransition>
