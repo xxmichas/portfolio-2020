9dd7af283f3393b0127f8ae0ffe0cea9577ef59f
Michal Orkisz <xxmichas@gmail.com>
2020-11-24 04:47:09 +0000

    projects continued

--- src/containers/ProjectCard/ProjectCard.js
+++ src/containers/ProjectCard/ProjectCard.js
@@ -2,8 +2,15 @@ import React from 'react'
 import styles from './ProjectCard.module.css'
 
 const ProjectCard = (props) => {
+    let centered = null
+    if (props.centered) {
+        centered = {
+            backgroundPositionX: "50%"
+        }
+    }
+
     return (
-        <div className={styles.Card} style={{backgroundImage: `url(${props.background})`}}>
+        <div className={styles.Card} style={{backgroundImage: `url(${props.background})`, ...centered}}>
             <div className={styles.Footer}>
                 <div className={styles.Title}>
                     {props.title}
--- src/containers/Section3/Section3.js
+++ src/containers/Section3/Section3.js
@@ -6,6 +6,9 @@ import styles from './Section3.module.css'
 import NTDF from '../../assets/projects/NTDF.png'
 import VR from '../../assets/projects/VR.png'
 import Portfolio from '../../assets/projects/portfolio.png'
+import SAU from '../../assets/projects/SAU.png'
+import Bot from '../../assets/projects/bot.png'
+import Server from '../../assets/projects/server.png'
 
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -22,12 +25,12 @@ const Section3 = (props) => {
                     slidesPerView={2}
                     navigation loop centeredSlides
                     onSlideChange={() => console.log('slide change')}>
-                    <SwiperSlide><ProjectCard background={NTDF} title="Nature TD: Forest" description="3D Unity Game" modal={null} /></SwiperSlide>
-                    <SwiperSlide><ProjectCard background={VR} title="Vertical Runner" description="2D Unity Game" modal={null} /></SwiperSlide>
-                    <SwiperSlide><ProjectCard background={null} title="Steam Achievement Unlocker" description="JavaScript Desktop App" modal={null} /></SwiperSlide>
-                    <SwiperSlide><ProjectCard background={null} title="Discord bot in Node.js" description="Discord Bot Using Discord.js Library" modal={null} /></SwiperSlide>
-                    <SwiperSlide><ProjectCard background={null} title="Web Server in Node.js" description="HTTP Server With Authentication And Tokens" modal={null} /></SwiperSlide>
-                    <SwiperSlide><ProjectCard background={Portfolio} title="Portfolio Website in React" description="My Portfolio Website Built With React" modal={null} /></SwiperSlide>
+                    <SwiperSlide><ProjectCard background={NTDF} centered={true} title="Nature TD: Forest" description="3D Unity Game" modal={null} /></SwiperSlide>
+                    <SwiperSlide><ProjectCard background={VR} centered={true} title="Vertical Runner" description="2D Unity Game" modal={null} /></SwiperSlide>
+                    <SwiperSlide><ProjectCard background={SAU} centered={false} title="Steam Achievement Unlocker" description="JavaScript Desktop App" modal={null} /></SwiperSlide>
+                    <SwiperSlide><ProjectCard background={Bot} centered={true} title="Discord bot in Node.js" description="Discord Bot Using Discord.js Library" modal={null} /></SwiperSlide>
+                    <SwiperSlide><ProjectCard background={Server} centered={false} title="Web Server in Node.js" description="HTTP Server With Authentication And Tokens" modal={null} /></SwiperSlide>
+                    <SwiperSlide><ProjectCard background={Portfolio} centered={true} title="Portfolio Website in React" description="My Portfolio Website Built With React" modal={null} /></SwiperSlide>
                 </Swiper>
             </div>
             <SectionTitle show={props.currentSection === 3 ? true : false} textLeft="My&nbsp;" textRight="Projects" />
