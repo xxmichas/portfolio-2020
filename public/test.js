7c35f02bf19c8cf6bdb6456026aa692a22577e33
Michał Orkisz <40291807+xxmichas@users.noreply.github.com>
2021-09-03 10:39:32 +0200

    Add files via upload

---
+++ public/test.js
@@ -0,0 +1,12 @@
+let getCookies = function(){
+  let pairs = document.cookie.split(";");
+  let cookies = {};
+  for (let i=0; i<pairs.length; i++){
+    let pair = pairs[i].split("=");
+    cookies[(pair[0]+'').trim()] = unescape(pair.slice(1).join('='));
+  }
+  return cookies;
+}
+
+let cookies = getCookies()
+console.log(cookies)
